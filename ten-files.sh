30f72c56ce70cc702c2eb3953a195e65e27fc825
Sam Nguyen <sjn909@gmail.com>
2016-10-12 20:50:13 -0500

    add script file

---
+++ ten-files.sh
@@ -0,0 +1,21 @@
+touch ~/test01/test01-question-five/crumpets01.txt
+echo ‘the crags are the best part!’ > ~/test01/test01-question-five/crumpets01.txt
+touch ~/test01/test01-question-five/crumpets02.txt
+echo ‘the crags are the best part!’ > ~/test01/test01-question-five/crumpets02.txt
+touch ~/test01/test01-question-five/crumpets03.txt
+echo ‘the crags are the best part!’ > ~/test01/test01-question-five/crumpets03.txt
+touch ~/test01/test01-question-five/crumpets04.txt
+echo ‘the crags are the best part!’ > ~/test01/test01-question-five/crumpets04.txt
+touch ~/test01/test01-question-five/crumpets05.txt
+echo ‘the crags are the best part!’ > ~/test01/test01-question-five/crumpets05.txt
+touch ~/test01/test01-question-five/crumpets06.txt
+echo ‘My present working directory is: your-pwd-here’ > touch ~/test01/test01-question-five/crumpets06.txt
+touch ~/test01/test01-question-five/crumpets07.txt
+echo ‘My present working directory is: your-pwd-here’ > touch ~/test01/test01-question-five/crumpets07.txt
+touch ~/test01/test01-question-five/crumpets08.txt
+echo ‘My present working directory is: your-pwd-here’ > touch ~/test01/test01-question-five/crumpets08.txt
+touch ~/test01/test01-question-five/crumpets09.txt
+echo ‘My present working directory is: your-pwd-here’ > touch ~/test01/test01-question-five/crumpets09.txt
+touch ~/test01/test01-question-five/crumpets10.txt
+echo ‘My present working directory is: your-pwd-here’ > touch ~/test01/test01-question-five/crumpets10.txt
+
